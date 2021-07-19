fc4fda8ddbf73863485d1a20582c503f4d3df37e
YasiruAyesh97 <yashiruayashmantha@gmail.com>
2021-07-19 10:01:15 +0530

    create studentService

--- src/main/java/com/relation/relationship/model/Student.java
+++ src/main/java/com/relation/relationship/model/Student.java
@@ -9,6 +9,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.relation.relationship.request.CreateStudentRequest;
+
 @Entity
 @Table(name = "student")
 public class Student {
@@ -29,4 +31,51 @@ public class Student {
 	@OneToOne
 	@JoinColumn(name = "address_id")
 	private Address address;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+	public Student (CreateStudentRequest createStudentRequest) {
+		this.firstName = createStudentRequest.getFirstName();
+		this.lastName = createStudentRequest.getLastName();
+		this.email = createStudentRequest.getEmail();
+	}
 }
--- src/main/java/com/relation/relationship/service/StudentService.java
+++ src/main/java/com/relation/relationship/service/StudentService.java
@@ -2,8 +2,11 @@ package com.relation.relationship.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.relation.relationship.model.Address;
+import com.relation.relationship.model.Student;
 import com.relation.relationship.repository.AddressRepository;
 import com.relation.relationship.repository.StudentRepository;
+import com.relation.relationship.request.CreateStudentRequest;
 
 public class StudentService {
 
@@ -13,4 +16,17 @@ public class StudentService {
 	@Autowired
 	AddressRepository addressRepository;
 	
+	public Student createStudent (CreateStudentRequest createStudentRequest) {
+		Student student = new Student(createStudentRequest);
+		
+		Address address = new Address();
+		address.setStreet(createStudentRequest.getStreet());
+		address.setCity(createStudentRequest.getCity());
+		
+		address = addressRepository.save(address);
+		
+		student.setAddress(address);
+		student = studentRepository.save(student);
+		return student;
+	}
 }
